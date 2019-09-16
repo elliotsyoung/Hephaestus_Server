e2dae126ddedc31504b6f52ff44c86a967cf4a18
Elliot Young <elliotsyoung@gmail.com>
2019-09-16 15:56:58 -0700

    created basic socket and server

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+app.set('port', process.env.PORT || 3000);
+const io = require(__dirname + '/socketio_custom_setup.js');
+app.get("/", (req, res) => {
+  res.send("Welcome to the Hephaestus Server");
+});
+server.listen(app.get('port'), function() {
+  console.log(`listening on ${app.get('port')}`);
+});
+io.attach(server); //activates websockets for server.
