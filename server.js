ae1b506c4a84d74ce6f42e470a42735fb4162a83
Justin Russell <justinrussell@Justins-MacBook-Pro.local>
2019-10-23 16:07:40 -0700

    update

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const app = express();
 const server = require('http').createServer(app);
 app.set('port', process.env.PORT || 3000);
 const io = require(__dirname + '/socketio_custom_setup.js');
-app.get("/", (req, res) => {
-  res.require(index.js);
-  //res.send('index.js');
-});
+module.exports.index = index;
+
+var index = function(req,res) {
+  res.render('index');
+}
+
 server.listen(app.get('port'), function() {
   console.log(`listening on ${app.get('port')}`);
 });
