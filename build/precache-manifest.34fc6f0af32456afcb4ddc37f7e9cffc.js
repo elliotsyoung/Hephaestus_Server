d4df714e0e8f08667c36c18311a27cf18cce2b32
Elliot Young <elliotsyoung@gmail.com>
2019-11-01 15:56:51 -0700

    updated with default react deployment

---
+++ build/precache-manifest.34fc6f0af32456afcb4ddc37f7e9cffc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d38e5d997be502cf298a162f529fd83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3069181f640fb1f2b822",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "9d827af7e564b1ca2ef5",
+    "url": "/static/js/2.dd91f5e1.chunk.js"
+  },
+  {
+    "revision": "3069181f640fb1f2b822",
+    "url": "/static/js/main.d014cce7.chunk.js"
+  },
+  {
+    "revision": "5895cef7185904880b4c",
+    "url": "/static/js/runtime-main.14c92e60.js"
+  }
+]);
--- server.js
+++ server.js
@@ -3,8 +3,11 @@ const app = express();
 const server = require('http').createServer(app);
 app.set('port', process.env.PORT || 3000);
 const io = require(__dirname + '/socketio_custom_setup.js');
+
+app.use(express.static(__dirname + "/build/static"))
+
 app.get("/", (req, res) => {
-  res.send('index.js');
+  res.sendFile(__dirname + '/build/index.html');
 });
 server.listen(app.get('port'), function() {
   console.log(`listening on ${app.get('port')}`);
