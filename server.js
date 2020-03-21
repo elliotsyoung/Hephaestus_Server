08a16698dd7f97ac88896a45760ecf36827ea437
Elliot Young <elliotsyoung@gmail.com>
2020-03-20 19:32:43 -0700

    created folder for public assets

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const io = require(__dirname + '/socketio_custom_setup.js');
 
 app.use(express.static(__dirname + "/build"))
 
+app.use("/public", express.static(__dirname + "/public"))
+
 server.listen(app.get('port'), function() {
   console.log(`listening on ${app.get('port')}`);
 });
